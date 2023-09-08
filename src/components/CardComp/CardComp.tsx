b411126b3abcd59f0d662fc8c7e55fadb2fa6fcf
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-09-08 15:06:07 +0300

    upload

--- src/components/CardComp/CardComp.tsx
+++ src/components/CardComp/CardComp.tsx
@@ -7,7 +7,7 @@ export default function CardComp({ title, data }: CardProps) {
     <MDBCard className="text-bg-dark h-100">
       <MDBCardBody>
         <MDBCardTitle className="mb-3 fs-4">{title}</MDBCardTitle>
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2">
           {data.map(({ id, img, title, to }) => {
             return (
               <div key={id} className="col text-center">
