e2a97d9a9aa4ca5cbf67df8d8007b98fbd18c71d
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-09-30 21:22:11 +0300

    refactor

--- src/pages/React/React.tsx
+++ src/pages/React/React.tsx
@@ -1,6 +1,6 @@
 import data from "./data.json";
 import MainPage from "../../components/MainPage/MainPage";
 
-export default function Technologies() {
+export default function React() {
   return <MainPage data={data} />;
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Technologies from "./pages/Technologies/Technologies";
 import Others from "./pages/Others/Others";
 import Work from "./pages/Work/Work";
 import Tools from "./pages/Tools/Ai";
 import Programming from "./pages/Programming/Programming";
 import WebDevelopment from "./pages/WebDevelopment/WebDevelopment";
+import React from "./pages/React/React";
 
 export const router = createBrowserRouter([
   {
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Technologies />,
+        element: <React />,
       },
       {
         path: "/tools",
